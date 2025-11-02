681f06b1e4d3d8242eb56bbb8d330836fdda0aa7
Prakhar-70 <prakharp192@gmail.com>
2025-11-02 12:33:15 +0530

    Update App.jsx

--- email-writer-frontend/src/App.jsx
+++ email-writer-frontend/src/App.jsx
@@ -1,98 +1,3 @@
-
-// import { Box, Container, TextField, Typography, FormControl, InputLabel, Select, MenuItem, Button, CircularProgress} from '@mui/material';
-// import './App.css';
-// import { useState } from 'react';
-// import axios from 'axios';
-
-
-// function App() {
-//   const [emailContent, setEmailContent] = useState('');
-//   const [tone, setTone] = useState('');
-//   const [loading, setLoading] = useState(false);
-//   const [generatedReply, setGeneratedReply] = useState('');
-
-//   const handleSubmit = async () => {
-//     setLoading(true);
-//    try {
-//       const response = await axios.post("http://localhost:8080/api/email/generate",{
-//         emailContent,
-//         tone
-//    });
-//       setGeneratedReply(typeof response.data === 'string' ?
-//         response.data : JSON.stringify(response.data)
-//       );
-//     } catch (error) {
-//        // Optionally, display an error message
-//     console.error("Failed to generate reply:", error);
-//     setGeneratedReply("Failed to generate reply.");
-      
-//     } finally{
-//       setLoading(false);
-//     }
-//   };
-  
-//   return (
-//     <>
-//        <Container maxWidth="md" sx={{py:4}}>
-//         <Typography variant='h3' component="h1" gutterBottom>
-//           Email Reply Generator
-//         </Typography>
-//         <Box sx={{mx:3}}>
-//           <TextField
-//            fullWidth
-//            multiline
-//            rows={6}
-//            variant='outlined'
-//            label = "Original Email Content"
-//            value={emailContent || ''}
-//            onChange={(e) => setEmailContent(e.target.value)}
-//            sx={{mb:2}}
-//           />
-//           <FormControl fullWidth sx={{mb:2}}>
-//             <InputLabel>Tone (Optional)</InputLabel>
-//             <Select
-//               value={tone || ''}
-//               label="Tone (Optional)"
-//               onChange={(e) => setTone(e.target.value)}
-//             >
-//               <MenuItem value="">None</MenuItem>
-//               <MenuItem value="professional">Professional</MenuItem>
-//               <MenuItem value="casual">Casual</MenuItem>
-//                <MenuItem value="friendly">Friendly</MenuItem>
-//             </Select>
-//           </FormControl>
-//           <Button variant="contained"
-//           sx={{mb:2}}
-//            onClick={handleSubmit} 
-//            disabled={!emailContent || loading}>
-//             {loading ? <CircularProgress size={24}/>: "Generate Reply"}
-//           </Button>
-//         </Box>
-//         <Box sx={{mx:3}}>
-//           <TextField
-//            fullWidth
-//            multiline
-//            rows={6}
-//            variant='outlined'
-//            value= {generatedReply || ''}
-//            InputProps={{readOnly:true}}
-//            sx={{mb:2}}
-//           />
-//            <Button
-//           variant='outlined'
-//           onClick={() => navigator.clipboard.write(generatedReply)}>
-//             Copy To Clipboard
-//           </Button>
-//           </Box>
-//        </Container>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
 import { Box, Container, TextField, Typography, FormControl, InputLabel, Select, MenuItem, Button, CircularProgress, Card, CardContent } from '@mui/material';
 import './App.css';
 import { useState } from 'react';
@@ -107,7 +12,7 @@ function App() {
   const handleSubmit = async () => {
     setLoading(true);
     try {
-      const response = await axios.post("http://localhost:8080/api/email/generate", {
+      const response = await axios.post("https://ai-email-assistant-l0g1.onrender.com", {
         emailContent,
         tone
       });

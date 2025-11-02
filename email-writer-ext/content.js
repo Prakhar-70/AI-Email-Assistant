4a8799222e69ebe3f89f4bd897009df6a0028730
Prakhar-70 <prakharp192@gmail.com>
2025-11-02 12:34:50 +0530

    Update content.js

--- email-writer-ext/content.js
+++ email-writer-ext/content.js
@@ -92,7 +92,7 @@ function injectButton() {
       button.style.opacity = '0.7';
       button.disabled = true;
 
-      const response = await fetch('http://localhost:8080/api/email/generate', {
+      const response = await fetch(' https://ai-email-assistant-l0g1.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
